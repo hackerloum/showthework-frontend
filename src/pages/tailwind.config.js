b69a535a1b3fffcde9d0ea11813f10eb15509b03
hackerloum <69545190+hackerloum@users.noreply.github.com>
2025-03-14 21:15:33 +0300

    Create tailwind.config.js

---
+++ src/pages/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // You can add custom colors here
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+} 
