130b945f9303512d9be6bd1c84d245a300fe2cee
hackerloum <69545190+hackerloum@users.noreply.github.com>
2025-03-14 16:22:01 +0300

    Update app.js

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Container } from '@mui/material';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// Components
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Images from './pages/Images';
+import Upload from './pages/Upload';
+
+// Create theme
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/images" element={<Images />} />
+            <Route path="/upload" element={<Upload />} />
+          </Routes>
+        </Container>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp; 
+export default App; 
